e31f6c6d5a930ab351ea3be3ee478b67a29bf3f7
Chris Pollard <cspollard@gmail.com>
2016-10-31 13:23:20 +0000

    some factorization

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE DeriveGeneric #-}
 
 module Main where
@@ -9,109 +10,20 @@ import Control.Applicative
 
 import Foreign.C.String
 
-import Data.Semigroup
 import Data.Maybe (fromMaybe)
-import Data.Vector (Vector)
 
 import qualified Data.ByteString.Lazy as BS
 import Data.Serialize (encodeLazy)
 import Data.Serialize.ZipList ()
 import Codec.Compression.GZip (compress)
 
-import GHC.Float
-
-import Data.YODA.Obj
-import Data.TTree
 import Data.TH1
-import Data.Atlas.Histogramming
 import Data.Atlas.CrossSections
 
 import Options.Generic
 
+import BCalib.Histograms
 
-newtype Filler m = Filler (ZipList (YodaObj -> TR m YodaObj), ZipList YodaObj)
-
-
-singleton :: (YodaObj -> TR m YodaObj) -> YodaObj -> Filler m
-singleton f h = Filler (ZipList [f], ZipList [h])
-
-fillH :: (MonadIO m, Foldable f) => TR m (f Double) -> TR m Double -> YodaObj -> Filler m
-fillH obs wgt = singleton f
-    where f h = do
-            xs <- obs
-            w <- wgt
-            return $ foldl (\h' x -> over (noted._H1DD) (filling w x) h') h xs
-
-fillP :: (MonadIO m, Foldable f) => TR m (f (Double, Double)) -> TR m Double -> YodaObj -> Filler m
-fillP obs wgt = singleton f
-    where f h = do
-            xs <- obs
-            w <- wgt
-            return $ foldl (\h' x -> over (noted._P1DD) (filling w x) h') h xs
-
-
-weight :: MonadIO m => TR m Double
-weight = float2Double . product
-    <$> sequence
-        [ readBranch "eventWeight"
-        , readBranch "MCEventWeight"
-        , readBranch "PileupWeight"
-        , readBranch "leptonSF"
-        , readBranch "trigSF"
-        ]
-
-
-fillWH :: (MonadIO m, Foldable f) => TR m (f Double) -> YodaObj -> Filler m
-fillWH tr = fillH tr weight
-
-fillWP :: (MonadIO m, Foldable f) => TR m (f (Double, Double)) -> YodaObj -> Filler m
-fillWP tr = fillP tr weight
-
-
-instance Semigroup (Filler m) where
-    (<>) = addF
-
-instance Monoid (Filler m) where
-    mempty = Filler (ZipList [], ZipList [])
-    mappend = (<>)
-
-
-addF :: Filler m -> Filler m -> Filler m
-addF (Filler (x, y)) (Filler (x', y')) = Filler (addZL x x', addZL y y')
-    where addZL (ZipList xs) (ZipList ys) = ZipList $ xs ++ ys
-
-
-runFiller :: MonadIO m => Filler m -> TTree -> m (ZipList YodaObj)
-runFiller (Filler (fs, hs)) = runTTree (ff fs) hs
-    where
-        ff fs' hs' = sequence $ fs' <*> hs'
-
-muH :: MonadIO m => Filler m
-muH = fillWH tr h
-    where
-        tr = Just . float2Double <$> readBranch "mu"
-        h = yodaHist 100 0 100 "/mu" "$ <\\mu> $" ""
-
-jetEtaH :: MonadIO m => Filler m
-jetEtaH = fillWH tr h
-    where
-        tr :: MonadIO m => TR m (Vector Double)
-        tr = fmap float2Double <$> readBranch "jetsMomEta"
-        h = yodaHist 100 (-3.0) 3.0 "/jeteta" "jet $\\eta$ [GeV]" ""
-
-jetPtH :: MonadIO m => Filler m
-jetPtH = fillWH tr h
-    where
-        tr :: MonadIO m => TR m (Vector Double)
-        tr = fmap float2Double <$> readBranch "jetsMomPt"
-        h = yodaHist 100 0 100000 "/jetpt" "jet $p_{\\mathrm T}$ [GeV]" ""
-
-hists :: MonadIO m => Filler m
-hists = mconcat
-    [ muH
-    , jetPtH
-    , jetEtaH
-    ]
 
 data Args = Args { outfile :: String
                  , infiles :: String
@@ -134,10 +46,10 @@ main = do
     hs <- forM fs $ \f -> do
         putStrLn $ "analyzing file " ++ f
         h <- withCString "MetaData_EventCount" $ \hn -> withCString f (th1d hn)
-        print =<< nbinsd h
+        ninitial <- entryd h 4
         t <- ttree "FlavourTagging_Nominal" f
-        runFiller hists t
+        (ninitial,) <$> runFiller bcalibHists t
 
-    let hs' = foldl1 (liftA2 mergeYO) hs
+    let hs' = foldl1 (\(n, ms) (n', ms') -> (n+n', liftA2 mergeYO ms ms')) hs
 
     BS.writeFile (outfile args) (compress $ encodeLazy hs')
---
+++ src/BCalib/Histograms.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module BCalib.Histograms
+    ( module X
+    , runFiller
+    , bcalibHists
+    ) where
+
+import Control.Lens
+import Control.Applicative
+
+import Data.Semigroup
+import Data.Vector (Vector)
+
+import GHC.Float
+
+import Data.YODA.Obj as X
+import Data.TTree as X
+import Data.Atlas.Histogramming as X
+
+newtype Filler m = Filler (ZipList (YodaObj -> TR m YodaObj), ZipList YodaObj)
+
+
+singleton :: (YodaObj -> TR m YodaObj) -> YodaObj -> Filler m
+singleton f h = Filler (ZipList [f], ZipList [h])
+
+fillH :: (MonadIO m, Foldable f) => TR m (f Double) -> TR m Double -> YodaObj -> Filler m
+fillH obs wgt = singleton f
+    where f h = do
+            xs <- obs
+            w <- wgt
+            return $ foldl (\h' x -> over (noted._H1DD) (filling w x) h') h xs
+
+fillP :: (MonadIO m, Foldable f) => TR m (f (Double, Double)) -> TR m Double -> YodaObj -> Filler m
+fillP obs wgt = singleton f
+    where f h = do
+            xs <- obs
+            w <- wgt
+            return $ foldl (\h' x -> over (noted._P1DD) (filling w x) h') h xs
+
+
+weight :: MonadIO m => TR m Double
+weight = float2Double . product
+    <$> sequence
+        [ readBranch "eventWeight"
+        , readBranch "MCEventWeight"
+        , readBranch "PileupWeight"
+        , readBranch "leptonSF"
+        , readBranch "trigSF"
+        ]
+
+
+fillWH :: (MonadIO m, Foldable f) => TR m (f Double) -> YodaObj -> Filler m
+fillWH tr = fillH tr weight
+
+fillWP :: (MonadIO m, Foldable f) => TR m (f (Double, Double)) -> YodaObj -> Filler m
+fillWP tr = fillP tr weight
+
+
+instance Semigroup (Filler m) where
+    (<>) = addF
+
+instance Monoid (Filler m) where
+    mempty = Filler (ZipList [], ZipList [])
+    mappend = (<>)
+
+
+addF :: Filler m -> Filler m -> Filler m
+addF (Filler (x, y)) (Filler (x', y')) = Filler (addZL x x', addZL y y')
+    where addZL (ZipList xs) (ZipList ys) = ZipList $ xs ++ ys
+
+
+runFiller :: MonadIO m => Filler m -> TTree -> m (ZipList YodaObj)
+runFiller (Filler (fs, hs)) = runTTree (ff fs) hs
+    where
+        ff fs' hs' = sequence $ fs' <*> hs'
+
+muH :: MonadIO m => Filler m
+muH = fillWH tr h
+    where
+        tr = Just . float2Double <$> readBranch "mu"
+        h = yodaHist 100 0 100 "/mu" "$ <\\mu> $" ""
+
+jetEtaH :: MonadIO m => Filler m
+jetEtaH = fillWH tr h
+    where
+        tr :: MonadIO m => TR m (Vector Double)
+        tr = fmap float2Double <$> readBranch "jetsMomEta"
+        h = yodaHist 100 (-3.0) 3.0 "/jeteta" "jet $\\eta$ [GeV]" ""
+
+jetPtH :: MonadIO m => Filler m
+jetPtH = fillWH tr h
+    where
+        tr :: MonadIO m => TR m (Vector Double)
+        tr = fmap float2Double <$> readBranch "jetsMomPt"
+        h = yodaHist 100 0 100000 "/jetpt" "jet $p_{\\mathrm T}$ [GeV]" ""
+
+bcalibHists :: MonadIO m => Filler m
+bcalibHists =
+    mconcat
+        [ muH
+        , jetPtH
+        , jetEtaH
+        ]
