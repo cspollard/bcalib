12b10c513dac796f9e842f52a85610800856d369
Chris Pollard <cspollard@gmail.com>
2016-11-07 12:59:22 +0000

    fixing up app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -42,38 +42,35 @@ main = do
 
     fns <- filter (not . null) . lines <$> readFile (infiles args)
 
-    -- TODO
-    -- this folding doesn't need to store histograms from each file
-    -- in memory...
-    -- TODO
-    -- TODO
-    -- turn this into a fold
-    -- that looks to see if we've already filled hists for this dsid
-    -- if so: continue filling the histograms
-    -- if not: use new fold.
     let collapse = IM.fromListWith (\(n, ms) (n', ms') -> (n+n', mergeYO <$> ms <*> ms'))
-    hs <- fmap collapse . forM fns $ \fn -> do
-        putStrLn ("analyzing file " ++ fn) >> hFlush stdout
 
-        let (dsid :: Int) =
-                if "data15_13TeV" `isInfixOf` fn || "data16_13TeV" `isInfixOf` fn
-                    then 0
-                    else fn
-                        & read . T.unpack . (!! 3)
-                        . T.split (== '.') . (!! 1)
-                        . reverse . T.split (== '/') . T.pack
-
-        let dsid' = if dsid < 300000 then 0 else dsid
-
-        f <- tfileOpen fn
-        h <- tfileGet f "MetaData_EventCount"
-        ninitial <- entryd h 4
-
-        t <- ttree f "FlavourTagging_Nominal"
-
-        nt <- isNullTree t
-        (fromEnum dsid',) . (ninitial,)
-            <$> F.purely L.fold (lepFlavorChannels . lepChargeChannels . nJetChannels $ eventHs) (if nt then L.empty else withWeight <$> project t)
-            <* tfileClose f
 
     BS.writeFile (outfile args) (compress . encodeLazy . over (traverse._2.traverse.path) ("/bcalib" <>) $ hs)
+
+    where
+        addFile fn im = do
+            putStrLn ("analyzing file " ++ fn) >> hFlush stdout
+
+            let (dsid :: Int) =
+                    if "data15_13TeV" `isInfixOf` fn || "data16_13TeV" `isInfixOf` fn
+                        then 0
+                        else fn
+                            & read . T.unpack . (!! 3)
+                            . T.split (== '.') . (!! 1)
+                            . reverse . T.split (== '/') . T.pack
+
+            f <- tfileOpen fn
+            h <- tfileGet f "MetaData_EventCount"
+            ninitial <- entryd h 4
+
+            t <- do
+                t' <- ttree f "FlavourTagging_Nominal"
+                if isNullTree t'
+                    then L.empty
+                    else withWeight <$> project t'
+
+            hs' <- (fromEnum dsid,) . (ninitial,)
+                <$> F.purely L.fold (lepFlavorChannels . lepChargeChannels . nJetChannels $ eventHs) t
+
+            tfileClose f
+            return hs'
