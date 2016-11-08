56a3e67307260f0714444f0450fe84f23b4c23ff
Chris Pollard <chris.pollard@duke.edu>
2016-11-08 15:19:46 +0100

    updating batch scripts

--- ext/runLXB.sh
+++ ext/runLXB.sh
@@ -2,6 +2,6 @@
 
 cd ~/Programming/bcalib.git
 TMP=`mktemp -d`
-bcalib --outfile $TMP/tmp --infiles $1 > ${1/infiles/log} 
+bcalibRun --outfile $TMP/tmp --infiles $1 > ${1/infiles/log} 
 mv $TMP/tmp ${1/infiles/hist.gz} 
 rm -r $TMP
