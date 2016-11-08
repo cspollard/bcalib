6ee47eff2f354dd029d1b3609f0c5e09cad12a41
Chris Pollard <chris.pollard@duke.edu>
2016-11-08 19:32:53 +0100

    make jobs crash if they fail.

--- ext/runLXB.sh
+++ ext/runLXB.sh
@@ -4,6 +4,7 @@ cd ~/Programming/bcalib.git
 TMP=`mktemp -d`
 bcalibRun --outfile $TMP/tmp --infiles $1 > ${1/infiles/log} 
 OUT=${1/infiles/hist.gz}`
-RTN=`mv $TMP/tmp $OUT`
+mv $TMP/tmp $OUT
+RTN=$!
 rm -r $TMP
 exit $RTN
