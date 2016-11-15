8dbbbd60d95ece6880c49f1dcfd501b2c4af42a0
Chris Pollard <chris.pollard@duke.edu>
2016-11-15 11:12:52 +0100

    fix in lxb script.

--- ext/runLXB.sh
+++ ext/runLXB.sh
@@ -7,8 +7,8 @@ lsetup ROOT
 echo "cd ~/Programming/bcalib.git"
 cd ~/Programming/bcalib.git
 
-echo "TMP=`mktemp -d`"
 TMP=`mktemp -d`
+echo "TMP=$TMP"
 
 echo "bcalibRun --outfile $TMP/tmp --infiles $1 > ${1/infiles/log}"
 bcalibRun --outfile $TMP/tmp --infiles $1 > ${1/infiles/log} 
@@ -16,7 +16,8 @@ bcalibRun --outfile $TMP/tmp --infiles $1 > ${1/infiles/log}
 echo "xrdcp $TMP/tmp root://eosatlas.cern.ch/$2"
 xrdcp $TMP/tmp root://eosatlas.cern.ch/$2
 
-RTN=$!
+RTN=$?
+
 echo "rm -r $TMP"
 rm -r $TMP
 
