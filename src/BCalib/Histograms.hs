4acb7eaee6dd0b6fa3f1f757715b3c396a3cd43e
Chris Pollard <cspollard@gmail.com>
2017-02-15 18:09:16 +0000

    Histograms compiling.

--- src/BCalib/Histograms.hs
+++ src/BCalib/Histograms.hs
@@ -16,22 +16,18 @@ module BCalib.Histograms where
     -- , lvHs, nH
     -- ) where
 
-import           Control.Applicative      as X (ZipList (..), liftA2)
 import qualified Control.Foldl            as F
 import           Control.Lens             as X
+import           Data.Bifunctor
 import qualified Data.Histogram.Generic   as G
 import qualified Data.Map.Strict          as M
-import           Data.Proxy
 import           Data.Semigroup           as X
 import           Data.Text                (Text)
 import qualified Data.Text                as T
 import qualified Data.Vector              as V
-import           GHC.TypeLits
-import           Linear.V
 
 import           Data.Atlas.Histogramming as X
 import           Data.HEP.LorentzVector   as X
-import           Data.TTree               as X
 import           Data.YODA.Obj            as X
 
 channel :: T.Text -> (a -> Bool) -> F.Fold a YodaFolder -> F.Fold a YodaFolder
@@ -41,9 +37,6 @@ channel n f fills = M.mapKeysMonotonic (n <>) <$> F.handles (selector f) fills
 channels :: [(T.Text, a -> Bool)] -> F.Fold a YodaFolder -> F.Fold a YodaFolder
 channels fns fills = mconcat $ uncurry channel <$> fns <*> pure fills
 
-
--- TODO
--- conversion from binning to ArbBin at the end of the day
 
 hEmpty :: (Bin bin, Monoid a) => bin -> Histogram V.Vector bin a
 hEmpty b =
@@ -71,35 +64,35 @@ prof1DDefault b xt yt pa =
       . over bins toArbBin
       <$> prof1DFill (hEmpty b)
 
-nH
-  :: Foldable f
-  -- => Int -> Hist1DFill (ArbBin Double) (f a, Double)
-  => Int -> F.Fold (f a, Double) YodaFolder
+type Fill a = F.Fold (a, Double) YodaFolder
+
+nH :: Foldable f => Int -> Fill (f a)
 nH n =
-  F.premap (over _1 (fromIntegral . length))
+  F.premap (first $ fromIntegral . length)
     $ hist1DDefault (binD 0 n (fromIntegral n)) "$n$" (dsigdXpbY "n" "1") "/n"
 
--- fillH1L (to $ fromIntegral.length) "/n" $
---     yodaHist mx 0 (fromIntegral mx) "$n$"
-
-selector :: (a -> Bool) -> Prism' a a
-selector f = prism' id $ \x -> if f x then Just x else Nothing
-
-{-
 ptH :: HasLorentzVector a => Fill a
-ptH = fillH1L lvPt "/pt" $
-    yodaHist 50 0 500 "$p_{\\mathrm T}$ [GeV]" (dsigdXpbY pt gev)
+ptH =
+  F.premap (first $ view lvPt)
+    $ hist1DDefault
+      (binD 0 50 500)
+      "$p_{\\mathrm T}$ [GeV]"
+      (dsigdXpbY pt gev)
+      "/pt"
 
 etaH :: HasLorentzVector a => Fill a
-etaH = fillH1L lvEta "/eta" $
-    yodaHist 30 (-3) 3 "$\\eta$" (dsigdXpbY "\\eta" "{\\mathrm rad}")
-
-infixl 2 <$$=
-(<$$=) :: Fill b -> Getter a b -> Fill a
-fs <$$= l = F.premap (fmap (view l)) fs
+etaH =
+  F.premap (first $ view lvEta)
+    $ hist1DDefault
+      (binD (-3) 39 3)
+      "$\\eta$"
+      (dsigdXpbY "\\eta" "{\\mathrm rad}")
+      "/etat"
 
 
 -- generic histograms for a lorentz vector
 lvHs :: HasLorentzVector a => Fill a
 lvHs = mappend <$> ptH <*> etaH
--}
+
+selector :: (a -> Bool) -> Prism' a a
+selector f = prism' id $ \x -> if f x then Just x else Nothing
