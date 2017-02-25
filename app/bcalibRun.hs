d0bb043308feb44958cfddbece5a5ac37dbd9460
Chris Pollard <cspollard@gmail.com>
2017-02-25 22:18:13 +0000

    some info is missing from my root files .....!!!!

--- app/bcalibRun.hs
+++ app/bcalibRun.hs
@@ -6,10 +6,9 @@
 module Main where
 
 import           Codec.Compression.GZip (compress)
-import           Control.Comonad        (Comonad (..))
 import qualified Control.Foldl          as F
 import           Control.Lens
-import           Control.Monad          (when)
+import           Control.Monad          (forM, when)
 import qualified Data.ByteString.Lazy   as BS
 import           Data.List              (isInfixOf)
 import qualified Data.Map.Strict        as M
@@ -41,13 +40,9 @@ main = do
 
   fns <- filter (not . null) . lines <$> readFile (infiles args)
   let fnl = L.select fns :: L.ListT IO String
-      f tn readws = F.FoldM (fillFile tn readws) (return Nothing) return
+      f = F.FoldM (fillFile allSysts) (return Nothing) return
 
-  (imf :: Maybe (Int, Double, F.Fold (Event, SystMap Double) (SystMap YodaFolder))) <-
-    F.impurely L.foldM (f "FlavourTagging_Nominal" weightSysts) fnl
-
-  -- let imf' = ifoldlOf traversed (\s m m' -> maybe m (M.union m') )
-  let imh = over (_Just._3') extract imf
+  imh <- F.impurely L.foldM f fnl
 
   putStrLn ("writing to file " ++ outfile args) >> hFlush stdout
 
@@ -55,57 +50,62 @@ main = do
 
 
 fillFile
-  :: String
-  -> TR IO (M.Map SystName Double)
-  -> Maybe (Int, Double, F.Fold (Event, SystMap Double) (SystMap YodaFolder))
+  :: [(TreeName, TR IO (M.Map SystName Double))]
+  -> Maybe (Int, Double, SystMap YodaFolder)
   -> String
-  -> IO (Maybe (Int, Double, F.Fold (Event, SystMap Double) (SystMap YodaFolder)))
-fillFile tn readws m fn = do
+  -> IO (Maybe (Int, Double, SystMap YodaFolder))
+fillFile systs m fn = do
   putStrLn ("analyzing file " ++ fn) >> hFlush stdout
 
   -- check whether or not this is a data file
   let (dsid :: Int) =
-          if "data15_13TeV" `isInfixOf` fn || "data16_13TeV" `isInfixOf` fn
-            then 0
-            -- this works on files of the form
-            -- /blah/blah/blah/blah.blah.blah.DSID.blah/blah
-            else
-              fn
-                & read . T.unpack . (!! 3)
-                  . T.split (== '.') . (!! 1)
-                  . reverse . T.split (== '/') . T.pack
+        if "data15_13TeV" `isInfixOf` fn || "data16_13TeV" `isInfixOf` fn
+          then 0
+          -- this works on files of the form
+          -- /blah/blah/blah/blah.blah.blah.DSID.blah/blah
+          else
+            fn
+              & read . T.unpack . (!! 3)
+                . T.split (== '.') . (!! 1)
+                . reverse . T.split (== '/') . T.pack
 
   f <- tfileOpen fn
   h <- tfileGet f "MetaData_EventCount"
   ninitial <- entryd h 4
 
-  t <- ttree f tn
+  systHs <- fmap M.unions . forM systs $ \(tn, readws) -> do
+    t <- ttree f tn
+    putStrLn $ "looping over tree " <> tn
 
-  -- deal with possible missing trees
-  nt <- isNullTree t
-  let l =
-        if nt
-          then (L.empty :: L.ListT IO (Event, SystMap Double))
-          else runTTreeL tmp t
-      tmp = do
-        evt <- overlapRemoval <$> fromTTree
-        if dsid == 0
-          then return (evt, M.singleton "data" 1)
-          else do
-            ws <- readws
-            return (evt, ws)
+    -- deal with possible missing trees
+    nt <- isNullTree t
+    when nt $ do
+      putStrLn $ "missing tree " <> tn <> " in file " <> fn <> "."
+      putStrLn "continuing."
+
+    let l =
+          if nt
+            then L.empty
+            else runTTreeL tmp t
+        tmp = do
+          evt <- overlapRemoval <$> fromTTree
+          liftIO $ print evt
+          if dsid == 0
+            then return (evt, M.singleton "data" 1)
+            else do
+              ws <- readws
+              liftIO $ print ws
+              return (evt, ws)
+
+    F.purely L.fold defHs l
+
+  tfileClose f
 
   case m of
-    Nothing -> do
-      hs <- F.purely L.fold (duplicate defHs) l <* tfileClose f
-      return (Just (dsid, ninitial, hs))
-
-    Just (dsid', n, hs') -> do
-      when (dsid /= dsid') $ error "attempting to analyze different dsids in one run!!!"
-
-      hs <- F.purely L.fold (duplicate hs') l <* tfileClose f
-      return (Just (dsid, n+ninitial, hs))
-
+      Nothing -> return $ Just (dsid, ninitial, systHs)
+      Just (dsid', n, hs') -> do
+        when (dsid /= dsid') $ error "attempting to analyze different dsids in one run!!!"
+        return $ Just (dsid, n+ninitial, M.unionWith mergeYF systHs hs')
 
   where
     defHs :: F.Fold (Event, SystMap Double) (SystMap YodaFolder)
--- src/BCalib/Systematics.hs
+++ src/BCalib/Systematics.hs
@@ -7,26 +7,24 @@ module BCalib.Systematics where
 import           Data.Map.Strict as M
 import           Data.Semigroup
 import qualified Data.Text       as T
+import           Debug.Trace
 import           GHC.Float
 
 import           Data.TTree
 
-type TreeName = T.Text
+type TreeName = String
 type SystName = T.Text
 type WeightName = String
 
 type SystMap = M.Map SystName
 
-data Systematic =
-  Systematic
-    { systName     :: SystName
-    , systTreeName :: TreeName
-    , systWeights  :: [WeightName]
-    } deriving (Show, Eq, Ord)
+nominalWeight :: MonadIO m => TR m Double
+nominalWeight = float2Double <$> readBranch "eventWeight"
+
 
 weightSysts :: MonadIO m => TR m (Map SystName Double)
 weightSysts = sequence
-  [ ("nominal", float2Double <$> readBranch "eventWeight")
+  [ ("nominal", nominalWeight)
   , ("pileup_up", puwup)
   ]
 
@@ -37,10 +35,25 @@ weightSysts = sequence
       wup <- head <$> readBranch "PileupWeightSys"
       return . float2Double $ w * wup / puw
 
-treeSysts :: Map SystName TreeName
-treeSysts =
-  fromList
-    $ f <$>
-      [ "JET_EffectiveNP_1__1up" ]
 
-  where f s = (s, "FlavourTagging_" <> s)
+treeSysts :: MonadIO m => [(TreeName, TR m (Map SystName Double))]
+treeSysts =
+  f <$>
+    [ "JET_EffectiveNP_1__1up"
+    , "JET_EffectiveNP_2__1up"
+    , "JET_EffectiveNP_3__1up"
+    , "JET_EffectiveNP_4__1up"
+    , "JET_EffectiveNP_5__1up"
+    , "JET_EffectiveNP_6restTerm__1up"
+    , "EL_EFF_ID_TotalCorrUncertainty__1up"
+    ]
+
+  where
+    f s =
+      ( "FlavourTagging_" <> s
+      , sequence $ M.singleton (T.pack s) (traceShow s . traceShowId <$> nominalWeight)
+      )
+
+
+allSysts :: MonadIO m => [(TreeName, TR m (Map SystName Double))]
+allSysts = ("FlavourTagging_Nominal", weightSysts) : treeSysts
